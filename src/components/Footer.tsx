78146a7f534cd45bd2c86f9171ef52341dcae9e5
Aleksa <stojsic.aleksa@gmail.com>
2022-09-12 15:42:53 +0200

    Fix: footer href

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -41,7 +41,7 @@ const Footer = () => {
         </a>{' '}
         ={' '}
         <a
-          href='https://www.github.com/aleksastojsic/nextjs-typscript-tailwind-starter'
+          href='https://www.github.com/aleksastojsic/typescript-next-tailwind-start'
           className='underline'
           target='_blank'
           rel='noopener noreferrer'
