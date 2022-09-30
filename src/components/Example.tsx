44997310a7d4a06f3fb4008a961602196f3933d9
aleksastojsic <stojsic.aleksa@gmail.com>
2022-09-30 02:21:54 +0200

    Fix: counter example component

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -6,18 +6,20 @@ export const Example = () => {
     <div className='w-72 rounded-xl border-2 border-black p-4 text-center'>
       <div className='text-2xl'>Example component</div>
       <p className='text-xl font-bold'>{count}</p>
-      <button
-        className='mr-2 rounded-lg bg-blue-500 py-1 px-4 font-bold text-white hover:bg-blue-700'
-        onClick={() => setCount(count + 1)}
-      >
-        +
-      </button>
-      <button
-        className='ml-2 rounded-lg bg-blue-500 py-1 px-4 font-bold text-white hover:bg-blue-700'
-        onClick={() => setCount(count - 1)}
-      >
-        -
-      </button>
+      <div className='flex justify-between px-16'>
+        <button
+          className='w-12 rounded-lg bg-blue-500 py-1  font-bold text-white hover:bg-blue-700'
+          onClick={() => setCount(count - 1)}
+        >
+          - 1
+        </button>
+        <button
+          className='w-12 rounded-lg bg-blue-500 py-1  font-bold text-white hover:bg-blue-700'
+          onClick={() => setCount(count + 1)}
+        >
+          + 1
+        </button>
+      </div>
     </div>
   );
 };
